4daecf89f8a71890a57bd58b94581792404aaa93
성은지 <96868722+eunji1@users.noreply.github.com>
2023-07-28 13:58:54 +0900

    context 삭제

--- ts-todolist/src/App.tsx
+++ ts-todolist/src/App.tsx
@@ -2,9 +2,8 @@ import { Provider } from "react-redux";
 import "./App.css";
 import AddTodo from "./components/AddTodo/AddTodo";
 import ThemeWrapper from "./components/ThemeWrapper";
-import Todos from "./containers/Todos";
+import Todos from "./components/Todos/Todos";
 import ThemeProvider from "./context/themeContext";
-import TodoProvider from "./context/todoContext";
 import React from "react";
 import store from "./redux/store";
 
@@ -12,15 +11,13 @@ function App() {
   return (
     <ThemeProvider>
       <Provider store={store}>
-        <TodoProvider>
-          <ThemeWrapper>
-            <main className="App">
-              <h1>My Todos</h1>
-              <AddTodo></AddTodo>
-              <Todos></Todos>
-            </main>
-          </ThemeWrapper>
-        </TodoProvider>
+        <ThemeWrapper>
+          <main className="App">
+            <h1>My Todos</h1>
+            <AddTodo></AddTodo>
+            <Todos></Todos>
+          </main>
+        </ThemeWrapper>
       </Provider>
     </ThemeProvider>
   );
